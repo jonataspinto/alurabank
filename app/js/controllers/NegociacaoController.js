89d1c1703db5cbd2eef9d9905f55514bfc28b870
Jonatas <jonatastdb_jp@hotmail.com>
2019-07-16 22:30:06 -0300

    reuse of code through inheritance.

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -2,6 +2,7 @@ class NegociacaoController {
     constructor() {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#negociacoesView');
+        this._mensagemView = new MensagemView('#mensagemView');
         this._inputData = document.querySelector('#data');
         this._inputQuantidate = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
@@ -19,5 +20,6 @@ class NegociacaoController {
             console.log(element.volume);
         });
         this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update("Negociação adicionada com sucesso!");
     }
 }
